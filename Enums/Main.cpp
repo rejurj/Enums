25194d19eabcfca40e0b43e25e78c4d277e03215
rejurj <rejujn@gmail.com>
2020-09-03 13:45:23 -0700

    Add project files.

---
+++ Enums/Main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+
+enum Example
+{
+	A =3, B, C
+};
+//int A = 3;
+//int B = 4;
+//int C = 5;
+int main()
+{
+	int value = B;
+	if (value = B)
+	{
+		//Do Something
+	}
+	std::cin.get();
+}
